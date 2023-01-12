e03aa7255fe45ee739f64bd8e87e342d33ce2aed
Allyson Araujo <allysoneugenio@gmail.com>
2023-01-12 10:37:35 -0300

    Atualização do nome da pasta

---
+++ src/one/digitalinnovation/Aula1_Criacao_Metodos/Emprestimo.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.Aula1_Criacao_Metodos;
+
+/*
+ * Classe de exemplo para o exercício da Aula 1 de Métodos
+ */
+public class Emprestimo {
+
+    public static int getDuasParcelas(){
+        return 2;
+    }
+
+    public static int getTresParcelas(){
+        return 3;
+    }
+
+    public static double getTaxaDuasParcelas(){
+        return 0.3;
+    }
+    
+}
